f123a8143e8dbe14509ec774c0dcab584f65ca8f
PrakhrS <prakhrsoni984@gmail.com>
2026-02-04 11:40:51 +0530

    Context API-1

---
+++ 08_miniContext/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContext from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+    
+      <h1>Hello World</h1>
+      <Login />
+      <Profile />
+    
+    </UserContextProvider>
+    
+  )
+}
+
+export default App
